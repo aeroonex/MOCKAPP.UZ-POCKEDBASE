fc123d49cf8499fcfe73936d053e1ac5b95a86e4
[dyad] <abdulmalikeraliyev08@gmail.com>
2026-01-24 16:35:41 +0500

    [dyad] Aylanuvchi matnga xiralashish effektini qo'shish - wrote 1 file(s)

--- src/components/RotatingText.tsx
+++ src/components/RotatingText.tsx
@@ -42,9 +42,9 @@ const RotatingText: React.FC<RotatingTextProps> = ({ type }) => {
       <AnimatePresence mode="wait">
         <motion.span
           key={currentIndex}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -20 }}
+          initial={{ opacity: 0, y: 20, filter: 'blur(5px)' }}
+          animate={{ opacity: 1, y: 0, filter: 'blur(0px)' }}
+          exit={{ opacity: 0, y: -20, filter: 'blur(5px)' }}
           transition={{ duration: 0.5, ease: "easeInOut" }}
           className="inline-block"
         >
